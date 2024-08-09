2c073cb849a5025e5c2898bf911ea4fbf642f89f
Rahul589950 <55463057+Rahul589950@users.noreply.github.com>
2024-08-09 17:11:48 -0400

    Update Question1.py
    Answers to Question 1

--- Question1.py
+++ Question1.py
@@ -2,3 +2,13 @@
 # then­else construct available in Python. (It is true that Python has the max()  function built in, but writing it
 # yourself is nevertheless a good exercise.)
 
+def max(num1, num2):
+    if (num1 <= num2):
+        return num2
+    else:
+        return num1
+    
+
+print(max(1,22))
+print(max(2, 0))
+
